aec20f0882f78dbb5601801f5d0ecc0417d7ff7b
DavidTMaciel <davidmacieljr122@gmail.com>
2023-07-04 20:57:57 -0300

    ajustes de importacao

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
 import { routes } from './routes/routes';
+import './database'
 
 const app = express();
 app.use(express.json());
 app.use(routes)
 
-app.listen(process.env.API_PORT, () =>console.log(`Server is running on port ${process.env.API_PORT}`));
+app.listen(process.env.API_PORT, () => console.log(`Server is running on port ${process.env.API_PORT}`));
